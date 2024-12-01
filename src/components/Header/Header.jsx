24f403207652c1b2bdad891291c431f63af39f63
Bibhu-shaw <118754564+Bibhu-shaw@users.noreply.github.com>
2024-12-01 12:02:37 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Welcome to Foodoor, your go-to destination for delicious meals delivered straight to your door! Whether you're craving a gourmet feast, a quick bite, or something in between, Foodoor has got you covered.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
